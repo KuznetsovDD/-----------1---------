2694213c3d77ee429229bed0983f6632942caca7
Atlant <cuz.den30@gmail.com>
2022-12-27 20:10:25 +0300

    исправления

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿
 System.Console.WriteLine("Введите значения массива через пробел: ");
-string str = Console.ReadLine()??"";
+string str = Console.ReadLine() ?? "";
 
 string[] array = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
 
 void PrintArray(string[] arr)
 {
-    for(int i=0; i<arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
         System.Console.Write($"{arr[i]} ");
     }
@@ -16,23 +16,23 @@ void PrintArray(string[] arr)
 
 string[] NewArray(string[] arra)
 {
-    string[] result = new string[new Random().Next(0,4)];
-    if(result.Length == 0)
+    string[] result = new string[new Random().Next(0, 4)];
+    if (result.Length == 0)
     {
         System.Console.WriteLine("нет значений");
         return result;
     }
     else
     {
-    for(int i=0; i< result.Length; i++)
-    {
-       result[i] = arra[new Random().Next(0, arra.Length)];
-    }
-    return result;
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = arra[new Random().Next(0, arra.Length)];
+        }
+        return result;
     }
 }
 
-string[] ar =   NewArray(array);
+string[] ar = NewArray(array);
 PrintArray(ar);
 
 
